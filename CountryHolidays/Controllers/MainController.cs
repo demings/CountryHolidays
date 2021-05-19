99014f3c45b2ce9ff748e53e8edb6276c0b9fca2
Mindaugas Laskis <mindaugas.laskis@gmail.com>
2021-05-19 23:29:29 +0300

    Implement DayStatus endpoint

--- CountryHolidays/Controllers/MainController.cs
+++ CountryHolidays/Controllers/MainController.cs
@@ -2,6 +2,7 @@
 using CountryHolidays.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -28,15 +29,27 @@ namespace CountryHolidays.Controllers
         [HttpGet("{code}/{year}")]
         public async Task<ActionResult<IEnumerable<IGrouping<int, Holiday>>>> GetHolidays(string code, int year)
         {
-            var holidays = await _context.Holidays.Where(h => h.Country.Code == code && h.Date.Year == year).ToListAsync();
+            var holidays = await _context.Holidays.Where(h => h.CountryCode == code && h.Date.Year == year).ToListAsync();
 
             return holidays.GroupBy(h => h.Date.Month).ToList();
         }
 
         [HttpGet("{code}/{year}/{month}/{day}")]
-        public async Task<ActionResult<IEnumerable<Country>>> GetDayStatus(string code, int year, int month, int day)
+        public async Task<ActionResult<string>> GetDayStatus(string code, int year, int month, int day)
         {
-            return await _context.Countries.ToListAsync();
+            var date = new DateTime(year, month, day);
+
+            var holiday = await _context.Holidays.Where(h => h.CountryCode == code && h.Date == date).FirstOrDefaultAsync();
+
+            if (holiday is null)
+            {
+                if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday)
+                    return DayStatus.Freeday.ToString();
+
+                return DayStatus.Workday.ToString();
+            }
+
+            return DayStatus.Holiday.ToString();
         }
 
         [HttpGet("{code}/{year}/max")]
